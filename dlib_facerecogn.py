f3969b5db64dd7234c8bc26a44e113c0a8745562
Fabrizio Tempo <58957501+Fabri000@users.noreply.github.com>
2024-04-19 17:01:24 +0200

    Update dlib_facerecogn.py

--- dlib_facerecogn.py
+++ dlib_facerecogn.py
@@ -7,20 +7,22 @@ from scipy.spatial import distance as dist
 from imutils import face_utils
 import numpy as np
 import cv2
+import time 
 
 def mouth_aspect_ratio(mouth):
 	# compute the euclidean distances between the two sets of
 	# vertical mouth landmarks (x, y)-coordinates
-    A = dist.euclidean(mouth[3], mouth[9]) # 52, 58
+    A = dist.euclidean(mouth[2], mouth[3]) # 52, 58
 	# compute the euclidean distance between the horizontal
 	# mouth landmark (x, y)-coordinates
-    C = dist.euclidean(mouth[0], mouth[6]) # 49, 55
+    C = dist.euclidean(mouth[0], mouth[1]) # 49, 55
 
 	# compute the mouth aspect ratio
     mar = A /  C
 
 	# return the mouth aspect ratio 
     return mar 
+
 
 
 def blur_image(img,k=50):
@@ -50,6 +52,8 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
 #Parameterss
 MOUTH_AR_THRESH=0.65
+MINIMUM_FACE_AREA = 5000
+MAXIMUM_FACE_AREA = 14000
 EPS=0.25
 
 predictor_path = 'shape_predictor_68_face_landmarks.dat'
@@ -62,41 +66,45 @@ predictor = dlib.shape_predictor(predictor_path)
 
 
 while(cap.isOpened()):
+    time.sleep(0.1)
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame = blur_image(frame)
     
     dets = detector(frame, 0)
-
     for k, d in enumerate(dets):
         
         shape = predictor(frame, d)
         shape = face_utils.shape_to_np(shape)
-
-        mouth = shape[mStart:mEnd]
-        face_borders = [shape[2],shape[14]]
         
-        distance = dist.euclidean(face_borders[0],face_borders[1])
-                
-        mouth_ar = mouth_aspect_ratio(mouth)
         
-        MAX_DISTANCE = frame.shape[0]//2
+        MAX_DISTANCE = frame.shape[0]//4
       
-        
-        corrected_mar = mouth_ar + (1 - distance/MAX_DISTANCE)*EPS
-        print(f'mount_ar: {mouth_ar}')
-        print(f'distance: {distance}')
-        print(f'MAX_DISTANCE: {MAX_DISTANCE} , distance/MAX_DISTANCE: {distance/MAX_DISTANCE},  (1 - distance/MAX_DISTANCE): {(1 - distance/MAX_DISTANCE)} f: {(1 - distance/MAX_DISTANCE)*EPS}')
-        print(f'correct mar : {corrected_mar}')
+        area =  d.area()
+        print(f'face area: {area}')
 
-        mouthHull = cv2.convexHull(mouth)
-        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-        cv2.putText(frame, "MAR: {:.2f}".format(mouth_ar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         
         # verifica se la bocca è aperta
-        if corrected_mar > MOUTH_AR_THRESH:
-            cv2.putText(frame, "Mouth is Open!", (30,60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
+        if (area >=MINIMUM_FACE_AREA) & (area <= MAXIMUM_FACE_AREA):
             
+            mouth = np.array([shape[60],shape[64],shape[62],shape[66]])
+            mouth_ar = mouth_aspect_ratio(mouth)
+
+            face_borders = [shape[2],shape[14]]
+            distance = dist.euclidean(face_borders[0],face_borders[1])
+
+            corrected_mar = mouth_ar + (1 - distance/MAX_DISTANCE)*EPS
+
+            print(f'mount_ar: {mouth_ar}')
+            print(f'distance: {distance}')
+            print(f'MAX_DISTANCE: {MAX_DISTANCE} , distance/MAX_DISTANCE: {distance/MAX_DISTANCE},  (1 - distance/MAX_DISTANCE): {(1 - distance/MAX_DISTANCE)} f: {(1 - distance/MAX_DISTANCE)*EPS}')
+            print(f'correct mar : {corrected_mar}')
+
+            mouthHull = cv2.convexHull(mouth)
+            cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+            cv2.putText(frame, "CORRECTED MAR: {:.2f}".format(corrected_mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            if corrected_mar > MOUTH_AR_THRESH:
+                cv2.putText(frame, "Mouth is Open!", (30,60),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
             
     cv2.imshow('frame', frame)
     
